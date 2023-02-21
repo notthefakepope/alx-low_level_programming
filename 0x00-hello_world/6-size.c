795f08c5ecd442266c1462e929ec715558e3c125
notthefakepope <samueljoshua615@gmail.com>
2023-02-20 18:21:41 -0800

    pope

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+printf("Size of a char: %zu byte(s)", sizeof(char));
+printf("Size of an int: %zu byte(s)", sizeof(int));
+printf("Size of a long int: %zu  byte(s)", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+printf("Size of a float: %zu byte(s)", sizeof(float));
+printf("\n");
+return (0);
+}
