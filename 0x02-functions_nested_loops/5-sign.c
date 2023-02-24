76fad8751bc067bbe1fec05c3ef15efe6cdce257
notthefakepope <samueljoshua615@gmail.com>
2023-02-24 15:02:13 -0800

    pope

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
  * print_sign - funcion that prints sign
@@ -15,22 +16,16 @@ int print_sign(int n)
 	{
 
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 }
